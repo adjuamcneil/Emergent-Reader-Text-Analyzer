9771f411021526eaf1e0f62e3a27984cd8721cb4
adjuamcneil <43587165+adjuamcneil@users.noreply.github.com>
2026-03-15 05:03:42 -0400

    Update textanalyzer_copy_paste_entry_theta.py
    Updated with appy.py head

--- textanalyzer_copy_paste_entry_theta.py
+++ textanalyzer_copy_paste_entry_theta.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
-
+import streamlit as st
 import pandas as pd
-from typing import Callable
 import spacy
-from collections import Counter
-import os
 import re
+from collections import Counter
+from pathlib import Path
+from typing import Callable
+streamlit
+pandas
+spacy
+numpy
+https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0.tar.gz
+
+# --- Configuration & Setup ---
+st.set_page_config(page_title="DHRI Text Analyzer", layout="wide")
+
+@st.cache_resource
 
 
 repo_root = Path("/https://github.com/adjuamcneil/Emergent-Reader-Text-Analyzer.git")  # adjust to your local clone
